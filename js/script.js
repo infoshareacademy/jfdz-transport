11decf659c03c95b402b21203cc67b3f0e77ad6c
kamilazabik <kamilazabik@gmail.com>
2016-01-23 22:03:34 +0100

    zmiana w menu i sekcji welcome

--- js/script.js
+++ js/script.js
@@ -12,3 +12,47 @@ function moveMouse() {
         document.getElementById("pole").innerHTML = z -= 1;
     }
 }
+
+(function() {
+
+    function createButton() {
+
+        var button = document.createElement("button");
+
+        button.classList.add("backToTop", "hidden");
+        //button.textContent = "Powrót do góry";
+        document.body.appendChild(button);
+
+        return button;
+
+    }
+
+    var button = createButton();
+
+    function animateScroll() {
+
+        if(document.body.scrollTop > 0) {
+            window.scrollBy(0, -5);
+            setTimeout(animateScroll, 3);
+        }
+    }
+
+    button.addEventListener("click", function(e){
+
+        e.stopPropagation();
+
+        animateScroll();
+
+    }, false);
+
+    window.addEventListener("scroll", function(e){
+
+        if(document.body.scrollTop >= 400) {
+            button.classList.remove("hidden");
+        }else {
+            button.classList.add("hidden");
+        }
+
+    }, false);
+
+})();
