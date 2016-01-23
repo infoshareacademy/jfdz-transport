2ca9d5a2881bbfbe0c6dcc68cbc86c719436a9c4
MarcinWirkus <marcin@inkre.pl>
2016-01-23 17:22:43 +0100

    formularz noBots

--- js/script.js
+++ js/script.js
@@ -2,3 +2,13 @@
  * Created by agnieszkacieslawska on 06.12.15.
  */
 
+var z = 100;
+function moveMouse() {
+
+    if (z == 0) {
+        document.getElementById("pole").innerHTML = 'Formularz odblokowany';
+        document.getElementById("przycisk").removeAttribute("disabled");
+    } else {
+        document.getElementById("pole").innerHTML = z -= 1;
+    }
+}
