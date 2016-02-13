ff7737aa9ef0b49b4ce1b263258856433e58ad01
M.Wirkus <marcin@inkre.pl>
2016-02-13 18:13:08 +0100

    gamebus v.0.03

--- js/gamebus.js
+++ js/gamebus.js
@@ -4,60 +4,77 @@ $(document).ready( function() {
 
     var losPrzystanku = Math.round(Math.random()*2);
 
+    var bs1 = $('#busstop1');
+    var bs2 = $('#busstop2');
+    var bs3 = $('#busstop3');
+
+    var czas = Math.round(Math.random()*5);
+
+    var pkt = 0;
+
+
+    setInterval( function odliczanie() {
+        if (czas>0) {
+            czas--;
+            console.log(czas);
+        } else {
+            $('.przystanek').off('click');
+            $('#punkty').html('Koniec! - Zdobyłeś punktów: '+pkt);
+        }
+          return;
+        },1000);
+
+
+
     if (losPrzystanku == 0)
     {
-        var pkt = 0;
-        var bs1 = $('#busstop1');
-        var czas = Math.round(Math.random()*20);
         bs1.addClass('aktywny');
         $('.aktywny').click(function () {
             pkt++;
             $('#busstop1 h2').html('Pasażerowie: '+pkt);
-            console.log(czas);
-            if (czas>0) {
-                czas--;
-            } else {
-                bs1.off('click');
-                $('h3').html('Koniec!');
-            }
             return;
         });
+
+        bs2.click(function () {
+           $(this).html('nie ten autobus');
+        });
+        bs3.click(function () {
+            $(this).html('nie ten autobus');
+        });
+
+
+
+
     } else if (losPrzystanku == 1) {
 
-        var pkt = 0;
-        var bs2 = $('#busstop2');
-        var czas = Math.round(Math.random()*20);
         bs2.addClass('aktywny');
         $('.aktywny').click(function () {
             pkt++;
             $('#busstop2 h2').html('Pasażerowie: '+pkt);
-            console.log(czas);
-            if (czas>0) {
-                czas--;
-            } else {
-                bs2.off('click');
-                $('h3').html('Koniec!');
-            }
             return;
+        });
+
+        bs1.click(function () {
+            $(this).html('nie ten autobus');
+        });
+        bs3.click(function () {
+            $(this).html('nie ten autobus');
         });
 
     } else {
 
-        var pkt = 0;
-        var bs3 = $('#busstop3');
-        var czas = Math.round(Math.random()*20);
         bs3.addClass('aktywny');
         $('.aktywny').click(function () {
             pkt++;
             $('#busstop3 h2').html('Pasażerowie: '+pkt);
-            console.log(czas);
-            if (czas>0) {
-                czas--;
-            } else {
-                bs3.off('click');
-                $('h3').html('Koniec!');
-            }
             return;
+        });
+
+        bs1.click(function () {
+            $(this).html('nie ten autobus');
+        });
+        bs2.click(function () {
+            $(this).html('nie ten autobus');
         });
 
     }
