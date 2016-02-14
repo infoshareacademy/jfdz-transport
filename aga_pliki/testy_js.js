1029342302dadb4c3c4ef6d7c9bc2486d2b3369b
AgnieszkaCie <acieslawska@gmail.com>
2016-02-14 21:33:25 +0100

    zadanka

--- aga_pliki/testy_js.js
+++ aga_pliki/testy_js.js
@@ -1,43 +1,58 @@
 /**
  * Created by agniecha on 2016-02-08.
  */
-var numer = Math.floor(Math.random() * 5) + 1;
+var numer = Math.floor(Math.random()*5)+1;
 
-function ustawslajd(nrslajdu){
-clearTimeout(timer1);
+var timer1 = 0;
+var timer2 = 0;
+
+function ustawslajd(nrslajdu)
+{
+    clearTimeout(timer1);
     clearTimeout(timer2);
+    numer = nrslajdu - 1;
+
     schowaj();
-    setTimeout()
-}
-function schowaj() {
-    $('#slajder').fadeOut(500);
-}
-function zmienslajd() {
-    numer++; if (numer > 5)numer = 1;
-    var plik = "<img src=\"../images/slajd" + numer + ".png\"/>";
-    document.getElementById('slajder').innerHTML = plik;
-    $('#slajder').fadeIn(300);
-    setTimeout('zmienslajd()', 300);
+    setTimeout("zmienslajd()", 500);
+
 }
 
-
-function odliczanie() {
-    var dzisiaj = new Date();
-
-    var dzien = dzisiaj.getDate();
-    var miesiac = dzisiaj.getMonth() + 1;
-    var rok = dzisiaj.getFullYear();
-    var godzina = dzisiaj.getHours();
-    if (godzina < 10)godzina = "0" + godzina;
-    var minuta = dzisiaj.getMinutes();
-    if (minuta < 10)minuta = "0" + minuta;
-    var sekunda = dzisiaj.getSeconds();
-    if (sekunda < 10)sekunda = "0" + sekunda;
-    document.getElementById('zegar').innerHTML = dzien + '/ ' + miesiac + '/ ' + rok + " " + " " + godzina + ": " + minuta + ": " + sekunda;
-    setTimeout('odliczanie()', 1000);
+function schowaj()
+{
+    $("#slider").fadeOut(500);
 }
 
-onload(odliczanie());
+function zmienslajd()
+{
+    numer++; if (numer>5) numer=1;
+
+    var plik = "<img src=\"../images/slajd" + numer + ".png\" />";
+
+    document.getElementById("slider").innerHTML = plik;
+    $("#slider").fadeIn(500);
+
+    timer1 = setTimeout("zmienslajd()", 5000);
+    timer2 = setTimeout("schowaj()", 4500);
+
+}
+
+//
+//function odliczanie() {
+//    var dzisiaj = new Date();
+//
+//    var dzien = dzisiaj.getDate();
+//    var miesiac = dzisiaj.getMonth() + 1;
+//    var rok = dzisiaj.getFullYear();
+//    var godzina = dzisiaj.getHours();
+//    if (godzina < 10)godzina = "0" + godzina;
+//    var minuta = dzisiaj.getMinutes();
+//    if (minuta < 10)minuta = "0" + minuta;
+//    var sekunda = dzisiaj.getSeconds();
+//    if (sekunda < 10)sekunda = "0" + sekunda;
+//    document.getElementById('zegar').innerHTML = dzien + ' / ' + miesiac + ' / ' + rok + " " + " " + godzina + ": " + minuta + ": " + sekunda;
+//    setTimeout('odliczanie()', 1000);
+//}
+//onload(odliczanie());
 
 function sprawdz() {
     var liczba = document.getElementById('pole').value;
@@ -57,7 +72,9 @@ function wypisz() {
     document.getElementById('ciagliczb').innerHTML = napis;
 }
 
-function addBox (){
-    document.getElementsByClassName('dodatkowybox').innerHTML +=document.getElementsByClassName('dodatkowybox').innerHtml + ""
-
+//function addBox (){
+//    document.getElementsByClassName('dodatkowybox').innerHTML += "dodatkowy tekst!!!!!!!!";
 }
+
+
+//document.querySelector("#content").getBoundingClientRect().top
