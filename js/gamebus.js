1c80e40f5ec6fbea49a598c88e3cdc8bd1b04fa2
MarcinWirkus <marcin@inkre.pl>
2016-02-20 18:44:23 +0100

    Poprawki w kodzie

--- js/gamebus.js
+++ js/gamebus.js
@@ -1,14 +1,12 @@
+$(document).ready(function () {
 
-
-$(document).ready( function() {
-
-    var losPrzystanku = Math.round(Math.random()*2);
+    var losPrzystanku = Math.round(Math.random() * 2);
 
     var bs1 = $('#busstop1');
     var bs2 = $('#busstop2');
     var bs3 = $('#busstop3');
 
-    var czas = Math.round(Math.random()*5);
+    var czas = Math.round(Math.random() * 5);
 
     var pkt = 0;
 
@@ -17,43 +15,36 @@ $(document).ready( function() {
     //for (; runda>0; runda--) {};
     //console.log('runda:'+runda);
 
-    setInterval( function odliczanie() {
+    setInterval(function odliczanie() {
+
+        if (czas > 0) {
+            czas--;
+            console.log(czas);
+        } else {
+            $('.przystanek').off('click');
+            $('#punkty').html('Koniec! - Zdobyłeś punktów: ' + pkt);
+        }
+        return;
+    }, 1000);
 
 
-            if (czas > 0) {
-                czas--;
-                console.log(czas);
-            } else {
-                $('.przystanek').off('click');
-                $('#punkty').html('Koniec! - Zdobyłeś punktów: ' + pkt);
-            }
-            return;
-        },1000);
-
-
-
-
-
-    if (losPrzystanku == 0)
-    {
+    if (losPrzystanku == 0) {
         bs1.addClass('aktywny');
         $('#busstop1 h2').html('Przyjechał autobus, kliknij i załaduj pasażerów ;)')
         $('.aktywny').click(function () {
             pkt++;
-            $('#busstop1 h2').html('Pasażerowie: '+pkt);
+            $('#busstop1 h2').html('Pasażerowie: ' + pkt);
             return;
         });
 
         bs2.click(function () {
             $(this).html('Auu! nie ten autobus - straciłeś -10 pkt!');
-           pkt = pkt - 10;
+            pkt = pkt - 10;
         });
         bs3.click(function () {
             $(this).html('Auu! nie ten autobus - straciłeś -10 pkt!');
             pkt = pkt - 10;
         });
-
-
 
 
     } else if (losPrzystanku == 1) {
@@ -62,7 +53,7 @@ $(document).ready( function() {
         $('#busstop2 h2').html('Przyjechał autobus, kliknij i załaduj pasażerów ;)')
         $('.aktywny').click(function () {
             pkt++;
-            $('#busstop2 h2').html('Pasażerowie: '+pkt);
+            $('#busstop2 h2').html('Pasażerowie: ' + pkt);
             return;
         });
 
@@ -81,7 +72,7 @@ $(document).ready( function() {
         $('#busstop3 h2').html('Przyjechał autobus, kliknij i załaduj pasażerów ;)')
         $('.aktywny').click(function () {
             pkt++;
-            $('#busstop3 h2').html('Pasażerowie: '+pkt);
+            $('#busstop3 h2').html('Pasażerowie: ' + pkt);
             return;
         });
 
