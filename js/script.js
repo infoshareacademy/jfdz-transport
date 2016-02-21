c8f1da4c3bb2bf0fc775f13054f61a82f23c82bd
kamila <kamilazabik@gmail.com>
2016-02-21 16:23:15 +0100

    przewijane i podswietlane menu

--- js/script.js
+++ js/script.js
@@ -87,7 +87,7 @@ $(document).ready(function(){
         $target = $(target);
         $('html, body').stop().animate({
             'scrollTop': $target.offset().top+2
-        }, 500, 'swing', function () {
+        }, 200, 'swing', function () {
             window.location.hash = target;
             $(document).on("scroll", onScroll);
         });
