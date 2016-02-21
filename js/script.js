9c9edc182e37e9bf8362570363f95d2ce07cac0e
MarcinWirkus <marcin@inkre.pl>
2016-02-21 16:48:27 +0100

    test-menu

--- js/script.js
+++ js/script.js
@@ -21,7 +21,7 @@ $(document).ready( function() {
 
     var z = 100;
 
-    $('#moveM').mousemove( function() {
+    $('#moveM').mousemove(function () {
         console.log('DZIALA');
 
 
@@ -30,43 +30,78 @@ $(document).ready( function() {
             $('#przycisk').removeAttr('disabled');
 
         } else {
-            $('#pole').html(z -= 1*10);
-            $('.mmwidth').width(z+'%');
+            $('#pole').html(z -= 1 * 10);
+            $('.mmwidth').width(z + '%');
         }
     });
 
-});
+    $(document).on("scroll", onScroll);
+
+    function onScroll(event) {
+        var scrollPos = $(document).scrollTop();
+        $('#menu-active a').each(function () {
+            var currLink = $(this);
+            var refElement = $(currLink.attr("href"));
+            if (refElement.position().top <= scrollPos && refElement.position().top + refElement.height() > scrollPos) {
+                $('#menu-active ul li a').removeClass("active");
+                currLink.addClass("active");
+            }
+            else {
+                currLink.removeClass("active");
+            }
+        });
+    }
+
+    $('a[href^="#"]').on('click', function (e) {
+        e.preventDefault();
+        $(document).off("scroll");
+
+        $('a').each(function () {
+            $(this).removeClass('active');
+        })
+        $(this).addClass('active');
+
+        var target = this.hash,
+            menu = target;
+        $target = $(target);
+        $('html, body').stop().animate({
+            'scrollTop': $target.offset().top + 2
+        }, 200, 'swing', function () {
+            window.location.hash = target;
+            $(document).on("scroll", onScroll);
+        });
+    });
 
 
+    $(document).ready(function () {
+        $(window).scroll(function () {
+            if ($(this).scrollTop() > 200) {
+                $('.scrollup').fadeIn();
+            } else {
+                $('.scrollup').fadeOut();
+            }
 
-$(document).ready(function() {
-    $(window).scroll(function(){
-        if($(this).scrollTop() > 200) {
-            $('.scrollup').fadeIn();
-        }else{
-            $('.scrollup').fadeOut();
-        }
+        })
 
-    })
+        $('.scrollup').click(function () {
+            $('html,body').animate({
+                scrollTop: 0
+            }, 600)
 
-    $('.scrollup').click(function(){
-        $('html,body').animate({
-            scrollTop: 0
-        }, 600)
+        })
 
-    })
+    });
 
-});
+    $(document).ready(function () {
+        $(window).scroll(function () {
+            if ($(this).scrollTop() > 50) {
+                $('header').addClass('naglowek-strony');
+            } else {
+                $('header').removeClass('naglowek-strony');
+            }
+        });
 
-$(document).ready(function(){
-    $(window).scroll(function() {
-        if($(this).scrollTop()> 50){
-            $('header').addClass('naglowek-strony');
-        } else {
-            $('header').removeClass('naglowek-strony');
-        }
-    })
-
+    });
 });
 
 //$(document).ready(function(){
@@ -86,58 +121,58 @@ $(document).ready(function(){
 //    })
 //});
 
-$(document).ready(function(){
-    var article = $('article'),
-        nav = $('nav'),
-        navHeight = nav.outerHeight();
-
-        //console.log(navHeight);
-
-    $(window).on('scroll', function() {
-        var currentPosition = $(this).scrollTop();
-
-        //console.log(currentPosition);
-
-        article.each(function(){
-            var top = $(this).offset().top - navHeight,
-                bottom = top + $(this).outerHeight();
-
-            //console.log(top);
-            //console.log(bottom);
-
-            if (currentPosition >= top && currentPosition <= bottom) {
-                nav.find('a').removeClass('activeNav');
-                article.removeClass('activeNav');
-
-                //$(this).addClass('activeNav');
-                //nav.find('a[href="#]'+$(this).attr('id')+'"]').addClass('activeNav');
-                var currentNav = nav.find('a[href^=#]');
-                console.log(currentNav);
-
-                var currentArticle = $(this).attr('id');
-                console.log(currentArticle);
-                if (currentNav == currentArticle) {
-                    currentNav.addClass('activeNav');
-                }
-
-
-            }
-
-        });
-
-        nav.find('a').on('click', function(){
-            var $el = $(this),
-                id = $el.attr('href');
-
-            $('html, body').animate({
-                scrollTop: $(id).offset().top - navHeight
-            }, 400);
-
-            return false;
-
-        })
-    })
-});
+//$(document).ready(function(){
+//    var article = $('article'),
+//        nav = $('nav'),
+//        navHeight = nav.outerHeight();
+//
+//        //console.log(navHeight);
+//
+//    $(window).on('scroll', function() {
+//        var currentPosition = $(this).scrollTop();
+//
+//        //console.log(currentPosition);
+//
+//        article.each(function(){
+//            var top = $(this).offset().top - navHeight,
+//                bottom = top + $(this).outerHeight();
+//
+//            //console.log(top);
+//            //console.log(bottom);
+//
+//            if (currentPosition >= top && currentPosition <= bottom) {
+//                nav.find('a').removeClass('activeNav');
+//                article.removeClass('activeNav');
+//
+//                //$(this).addClass('activeNav');
+//                //nav.find('a[href="#]'+$(this).attr('id')+'"]').addClass('activeNav');
+//                var currentNav = nav.find('a[href^=#]');
+//                console.log(currentNav);
+//
+//                var currentArticle = $(this).attr('id');
+//                console.log(currentArticle);
+//                if (currentNav == currentArticle) {
+//                    currentNav.addClass('activeNav');
+//                }
+//
+//
+//            }
+//
+//        });
+//
+//        nav.find('a').on('click', function(){
+//            var $el = $(this),
+//                id = $el.attr('href');
+//
+//            $('html, body').animate({
+//                scrollTop: $(id).offset().top - navHeight
+//            }, 400);
+//
+//            return false;
+//
+//        })
+//    })
+//});
 
 
 
