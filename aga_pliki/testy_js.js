5dedab8a29455c03034e355b04e5632bfff25ad1
AgnieszkaCie <acieslawska@gmail.com>
2016-02-22 09:53:07 +0100

    dalsze proby okno do gry

--- aga_pliki/testy_js.js
+++ aga_pliki/testy_js.js
@@ -1,95 +1,99 @@
-/**
- * Created by agniecha on 2016-02-08.
- */
-var numer = Math.floor(Math.random()*5)+1;
-var timer1 = 0;
-var timer2 = 0;
-
-function ustawslajd(nrslajdu)
-{
-    clearTimeout(timer1);
-    clearTimeout(timer2);
-    numer = nrslajdu - 1;
-
-    schowaj();
-    setTimeout("zmienslajd()", 500);
-
-}
-
-function schowaj()
-{
-    $("#slider").fadeOut(500);
-}
-
-function zmienslajd()
-{
-    numer++; if (numer>5) numer=1;
-
-    var plik = "<img src=\"slajd" + numer + ".png\" />";
-
-    document.getElementById("slider").innerHTML = plik;
-    $("#slider").fadeIn(500);
-
-    timer1 = setTimeout("zmienslajd()", 5000);
-    timer2 = setTimeout("schowaj()", 4500);
-
-}
-
-
-function odliczanie() {
-    var dzisiaj = new Date();
-
-    var dzien = dzisiaj.getDate();
-    var miesiac = dzisiaj.getMonth() + 1;
-    var rok = dzisiaj.getFullYear();
-    var godzina = dzisiaj.getHours();
-    if (godzina < 10)godzina = "0" + godzina;
-    var minuta = dzisiaj.getMinutes();
-    if (minuta < 10)minuta = "0" + minuta;
-    var sekunda = dzisiaj.getSeconds();
-    if (sekunda < 10)sekunda = "0" + sekunda;
-    document.getElementById('zegar').innerHTML = dzien + ' / ' + miesiac + ' / ' + rok + " " + " " + godzina + ": " + minuta + ": " + sekunda;
-    setTimeout('odliczanie()', 1000);
-}
-onload(odliczanie());
-
-function sprawdz() {
-    var liczba = document.getElementById('pole').value;
-    if (liczba > 0) document.getElementById('wynik').innerHTML = 'dodatnia';
-    else if (liczba < 0) document.getElementById('wynik').innerHTML = 'ujemna';
-    else if (liczba == 0)document.getElementById('wynik').innerHTML = "zero";
-    else document.getElementById('wynik').innerHTML = "to nie jest liczba";
-}
-
-function wypisz() {
-    var liczba1 = document.getElementById('pole1').value;
-    var liczba2 = document.getElementById('pole2').value;
-    var napis = "";
-    for (i = liczba1; i <= liczba2; i++) {
-        napis = napis + i + " ";
-    }
-    document.getElementById('ciagliczb').innerHTML = napis;
-}
-
-//$(".dodatkowybox").click(function(){
-//    //$('html,body').animate({
-//    //scrollTop: $("#ciagliczb").offset().top -500},
-//    //    'slow');
-//    document.getElementsByClassName('dodatkowybox').style.display="block"});
+///**
+// * Created by agniecha on 2016-02-08.
+// */
+//var numer = Math.floor(Math.random()*5)+1;
+//var timer1 = 0;
+//var timer2 = 0;
+//
+//function ustawslajd(nrslajdu)
+//{
+//    clearTimeout(timer1);
+//    clearTimeout(timer2);
+//    numer = nrslajdu - 1;
+//
+//    schowaj();
+//    setTimeout("zmienslajd()", 500);
+//
+//}
+//
+//function schowaj()
+//{
+//    $("#slider").fadeOut(500);
+//}
+//
+//function zmienslajd()
+//{
+//    numer++; if (numer>5) numer=1;
+//
+//    var plik = "<img src=\"slajd" + numer + ".png\" />";
+//
+//    document.getElementById("slider").innerHTML = plik;
+//    $("#slider").fadeIn(500);
+//
+//    timer1 = setTimeout("zmienslajd()", 5000);
+//    timer2 = setTimeout("schowaj()", 4500);
+//
+//}
+//
+//
+//function odliczanie() {
+//    var dzisiaj = new Date();
+//
+//    var dzien = dzisiaj.getDate();
+//    var miesiac = dzisiaj.getMonth() + 1;
+//    var rok = dzisiaj.getFullYear();
+//    var godzina = dzisiaj.getHours();
+//    if (godzina < 10)godzina = "0" + godzina;
+//    var minuta = dzisiaj.getMinutes();
+//    if (minuta < 10)minuta = "0" + minuta;
+//    var sekunda = dzisiaj.getSeconds();
+//    if (sekunda < 10)sekunda = "0" + sekunda;
+//    document.getElementById('zegar').innerHTML = dzien + ' / ' + miesiac + ' / ' + rok + " " + " " + godzina + ": " + minuta + ": " + sekunda;
+//    setTimeout('odliczanie()', 1000);
+//}
+//onload(odliczanie());
+//
+//function sprawdz() {
+//    var liczba = document.getElementById('pole').value;
+//    if (liczba > 0) document.getElementById('wynik').innerHTML = 'dodatnia';
+//    else if (liczba < 0) document.getElementById('wynik').innerHTML = 'ujemna';
+//    else if (liczba == 0)document.getElementById('wynik').innerHTML = "zero";
+//    else document.getElementById('wynik').innerHTML = "to nie jest liczba";
+//}
+//
+//function wypisz() {
+//    var liczba1 = document.getElementById('pole1').value;
+//    var liczba2 = document.getElementById('pole2').value;
+//    var napis = "";
+//    for (i = liczba1; i <= liczba2; i++) {
+//        napis = napis + i + " ";
+//    }
+//    document.getElementById('ciagliczb').innerHTML = napis;
+//}
 
 $("#dodatkowybox").click(function(){
     $('html,body').animate({
-        scrollTop: $("#ciagliczb").offset().top-100},'slow');
-    document.getElementById("dodatkowybox").style.display = "block";
-    $(this).addClass("on");
-    //document.getElementById('dodatkowybox').addClass = 'on';
-    //startGame();
+        scrollTop: $("#slider").offset().top -10},'slow');
+    document.getElementById("slider").style.display = "block";
+    $("#slider").addClass("on");
+    //$("#dodatkowybox").addClass("resetButton");
 });
 
-$("#resetButton").click(function(){
-      $().removeClass('on');
- });
+//$("#dodatkowybox").onmousemove(function(){
+//    $("#dodatkowybox").addClass("resetButton");
 
+    //$("#dodatkowybox").parent('div').removeClass("on");
+    //$("#dodatkowybox").parent('div').removeClass("on");
+//});
+//<div class="bold">
+//    <p id="p1" class="blue under">Hello</p>
+//    </div>
+//    <div class="bold">
+//    <p  id="p2" class="blue under highlight">and</p>
+//    </div>
+//    <p class="blue under">then</p>
+//    <p class="blue under">Goodbye</p>
+//    $("#p1").parent().removeClass("bold");
 //startGame();
 //document.querySelector("#content").getBoundingClientRect().top
 //var pozycja = $("#content").position()
