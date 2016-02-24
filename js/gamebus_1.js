fe639f51694f24083743f8e58e50890c8ccd3c54
kamilazabik <kamilazabik@gmail.com>
2016-02-24 22:58:17 +0100

    gra - pierwsze podejście

--- js/gamebus_1.js
+++ js/gamebus_1.js
@@ -1,59 +1,121 @@
 $(document).ready(function() {
 
+    gameOver = false;
+
     var bus1 = {
-      number: '1',
-      timer: '',
-      passengers: '',
-      points: ''
+        number: 1,
+        timer: '',
+        passengers: '',
+        points: ''
+
     };
+
+    var bus2 = {
+        number: 2,
+        timer: '',
+        passengers: '',
+        points: ''
+    };
+
+    var bus3 = {
+        number: 3,
+        timer: '',
+        passengers: '',
+        points: ''
+    };
+
+    bus1.Id = $('#bus1');
+    bus2.Id = $('#bus2');
+    bus3.Id = $('#bus3');
 
     var busstop = {
         number: '1',
         ifThereisBus: false
     };
 
-    var count = 0;
+    buses = [bus1, bus2, bus3];
+
+    gameOver = false;
+    var button = $('input');
 
     function setTime() {
 
-        var sec = $('.second').text()
+        var sec = parseInt($('.second').text());
         var timer = setInterval(function () {
             $('.second').text(--sec);
             if (sec == 0) {
                 $('.second').fadeOut('fast');
                 $('.finish').delay(500).fadeIn(500);
 
-                //clearInterval(timer);
+                clearInterval(timer);
+                gameOver = true;
             }
 
         }, 1000);
     }
 
-$('input').on('click', setTime);
+    button.on('click', setTime);
+
+    var count = 0;
 
 
+    var getBus = Math.floor((Math.random() * 3) + 1);
 
-        $('#busstop1').on('click', function(){
+    if (getBus == bus1.number) {
+
+        bus1.Id.addClass('bus-active');
+        bus2.Id.addClass('bus-no-active');
+        bus3.Id.addClass('bus-no-active');
+
+        $('.bus-active').on('click', function () {
             count++;
-           $('#points').html("Punkty: " + count);
+            $('#points').html("Punkty: " + count);
         });
 
-        $('#busstop2').on('click', function(){
-            count-= 10;
+        $('.bus-no-active').on('click', function () {
+            count -= 10;
             $('#points').html("Punkty: " + count);
         });
 
 
-    function getBus() {
-        x1 = Math.floor((Math.random() * 3) + 1);
-        console.log(x1);
-        return x1;
+    } else if (getBus == bus2.number) {
+
+        bus2.Id.addClass('bus-active');
+        bus1.Id.addClass('bus-no-active');
+        bus3.Id.addClass('bus-no-active');
+
+        $('.bus-active').on('click', function () {
+            count++;
+            $('#points').html("Punkty: " + count);
+        });
+
+        $('.bus-no-active').on('click', function () {
+            count -= 10;
+            $('#points').html("Punkty: " + count);
+        });
+
+
+    } else {
+        bus3.Id.addClass('bus-active');
+        bus2.Id.addClass('bus-no-active');
+        bus1.Id.addClass('bus-no-active');
+
+        $('.bus-active').on('click', function () {
+            count++;
+            $('#points').html("Punkty: " + count);
+        });
+
+        $('.bus-no-active').on('click', function () {
+            count -= 10;
+            $('#points').html("Punkty: " + count);
+        });
+
     }
 
-    if (x1 = bus1.number) {
-        $('#bus1').addClass('bus-color');
-    } else {
-        $('#bus1').removeClass('bus-color');
+
+
+    function losujAutobus (){
+        setInterval(getBus, 1000)
     }
 
     function setBusStop() {
@@ -61,8 +123,30 @@ $('input').on('click', setTime);
         console.log(x2);
     }
 
+
+
+
+
 });
 
+//setInterval(function() {
+//    for(var i = 1; i < 5; i++) {
+//        console.log(i);
+//    }}, 1000);
+//
+
+
+//
+//function setTime() {
+//    var time = parseInt($('.second').text());
+//
+//    if (time - 1 > -1) {
+//        $('.second').text(time - 1);
+//        return false;
+//    }
+//    window.clearInterval(time);
+//    gameOver = true;
+//}
 //function getBus() {
 //
 //}
