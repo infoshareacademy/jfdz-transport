bb3574145b03025cc7cc5d03d443be61e59a511e
kamilazabik <kamilazabik@gmail.com>
2016-02-28 19:02:24 +0100

    gra - kolejne podejście....

--- js/gamebus_1.js
+++ js/gamebus_1.js
@@ -38,9 +38,9 @@ $(document).ready(function() {
     gameOver = false;
     var button = $('input');
 
-    var howLongBusStop1 = Math.floor((Math.random() * 5) + 3);
-    var howLongBusStop2 = Math.floor((Math.random() * 5) + 3);
-    var howLongBusStop3 = Math.floor((Math.random() * 5) + 3);
+    var howLongBusStop1 = Math.floor((Math.random() * 5) + 1);
+    var howLongBusStop2 = Math.floor((Math.random() * 5) + 1);
+    var howLongBusStop3 = Math.floor((Math.random() * 5) + 1);
 
     //
     //function setBusTime(howLongBusStop) {
@@ -83,7 +83,7 @@ $(document).ready(function() {
 
     //bus1.timer = setBusTime(howLongBusStop1);
 
-    function jakas() {
+    function losowanieAutobusow() {
 
 
         var getBus = Math.floor((Math.random() * 5) + 1);
@@ -91,62 +91,82 @@ $(document).ready(function() {
         if (getBus == bus1.number) {
 
             bus1.Id.addClass('bus-active');
-            setTimeout(function(){bus1.Id.removeClass('bus-active');}, howLongBusStop1*1000);
+            bus1.Id.removeClass('bus-no-active');
+
+            setTimeout(function(){
+                bus1.Id.removeClass('bus-active');
+                bus1.Id.addClass('bus-no-active');
+            }, howLongBusStop1*1000);
+
             bus2.Id.addClass('bus-no-active').removeClass('bus-active');
             bus3.Id.addClass('bus-no-active').removeClass('bus-active');
 
 
-            $('.bus-active').on('click', function () {
-                count++;
-                $('#points').html("Punkty: " + count);
-            });
-
-            $('.bus-no-active').on('click', function () {
-                count -= 10;
-                $('#points').html("Punkty: " + count);
-            });
+            //$('.bus-active').on('click', function () {
+            //    count++;
+            //    $('#points').html("Punkty: " + count);
+            //});
+            //
+            //$('.bus-no-active').on('click', function () {
+            //    count -= 10;
+            //    $('#points').html("Punkty: " + count);
+            //});
 
 
         } else if (getBus == bus2.number) {
 
             bus2.Id.addClass('bus-active');
-            setTimeout(function(){bus2.Id.removeClass('bus-active');}, howLongBusStop2*1000);
+            bus2.Id.removeClass('bus-no-active');
+
+            setTimeout(function(){
+                bus2.Id.removeClass('bus-active');
+                bus2.Id.addClass('bus-no-active');
+            }, howLongBusStop2*1000);
+
             bus1.Id.addClass('bus-no-active').removeClass('bus-active');
             bus3.Id.addClass('bus-no-active').removeClass('bus-active');
 
-            $('.bus-active').on('click', function () {
-                count++;
-                $('#points').html("Punkty: " + count);
-            });
-
-            $('.bus-no-active').on('click', function () {
-                count -= 10;
-                $('#points').html("Punkty: " + count);
-            });
-
-
         } else {
             bus3.Id.addClass('bus-active');
-            setTimeout(function(){bus3.Id.removeClass('bus-active');}, howLongBusStop3*1000);
+            bus3.Id.removeClass('bus-no-active');
+
+            setTimeout(function(){
+                bus3.Id.removeClass('bus-active');
+                bus3.Id.addClass('bus-no-active');
+            }, howLongBusStop3*1000);
+
             bus2.Id.addClass('bus-no-active').removeClass('bus-active');
             bus1.Id.addClass('bus-no-active').removeClass('bus-active');
-
-            $('.bus-active').on('click', function () {
-                count++;
-                $('#points').html("Punkty: " + count);
-            });
-
-            $('.bus-no-active').on('click', function () {
-                count -= 10;
-                $('#points').html("Punkty: " + count);
-            });
 
         }
         //setTimeout(jakas, 3000);
 
+        //$('.bus-active').on('click', function () {
+        //        count++;
+        //        $('#points').html("Punkty: " + count);
+        //    });
+        //
+        //    $('.bus-no-active').on('click', function () {
+        //        count -= 10;
+        //        $('#points').html("Punkty: " + count);
+        //    });
+
     }
 
-    isetInterval(jakas, 1000);
+    setInterval(losowanieAutobusow, 3000);
+
+
+        $('div .bus-active').on('click', function () {
+            count +=1;
+            $('#points').html("Punkty: " + count);
+        });
+
+
+        $('.bus-no-active').on('click', function () {
+            count -= 10;
+            $('#points').html("Punkty: " + count);
+        });
+
 
 });
 
