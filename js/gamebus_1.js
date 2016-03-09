aca6994ea6c161d4a9f7747c7e581e993fc85a28
kamilazabik <kamilazabik@gmail.com>
2016-03-09 19:10:17 +0100

    commit

--- js/gamebus_1.js
+++ js/gamebus_1.js
@@ -27,145 +27,62 @@ $(document).ready(function() {
     bus1Id = $('#bus1');
     bus2Id = $('#bus2');
     bus3Id = $('#bus3');
+    $clock = $('#clock');
+    $start = $('#startGameButton');
 
     var busstop = {
         number: '1',
         ifThereisBus: false
     };
 
+    var state = {
+        score: 0,
+        time: 15
+    };
+
     buses = [bus1, bus2, bus3];
 
     gameOver = false;
-    var button = $('input');
     var count = 0;
 
-
-    function goToBusStation1() {
-        bus1Id.animate({
-            left: 200
-        }, bus1.timer, 'linear', goBack1);
-        setTimeout(goToBusStation1, 2000);
-    }
-    function goBack1() {
-        bus1Id.animate({
-            left:400
-        }, bus1.timer, 'linear');
-        bus1Id.animate({
-            left: 0
-        }, 2000, 'linear');
+    function displayClock(node, state) {
+        $(node).text('Czas: ' + state.time);
     }
 
 
-    function goToBusStation2() {
-        bus2Id.animate({
-            left: 200
-        }, bus2.timer, 'linear', goBack2);
-        setTimeout(goToBusStation2, 2000);
-    }
-    function goBack2() {
-        bus2Id.animate({
-            left:400
-        }, bus2.timer, 'linear');
-        bus2Id.animate({
-            left: 0
-        }, 2000, 'linear');
-    }
+function startGame(initialState) {
+    var clockIntervalId = setInterval(function () {
+        state.time -= 1;
+        displayClock($clock, state);
+    }, 1000);
 
-    function goToBusStation3() {
-        bus3Id.animate({
-            left: 200
-        }, bus3.timer, 'linear', goBack3);
-        setTimeout(goToBusStation3, 2000);
-    }
-    function goBack3() {
-        bus3Id.animate({
-            left:400
-        }, bus3.timer, 'linear');
-        bus3Id.animate({
-            left: 0
-        }, 2000, 'linear');
-    }
+    setTimeout(function () {
+        clearInterval(clockIntervalId);
+        //$('table').off('click');
+    }, state.time * 1000);
 
+}
 
-    goToBusStation1();
-    goToBusStation2();
-    goToBusStation3();
+    $start.click(function(){
+        startGame(state);
+    });
+
+    displayClock($clock, state);
 
 
 
+    var $appContainer = $('<div id="app">').css({position: 'relative'}).addClass('plansza');
+    var $busstop1 = $('<div id="busstop">').addClass('przystanek').addClass('przystanek1');
+    var $busstop2 = $('<div id="busstop">').addClass('przystanek').addClass('przystanek2');
+    var $busstop3 = $('<div id="busstop">').addClass('przystanek').addClass('przystanek3');
 
 
-    function setTime() {
+    $('body').append($appContainer);
+    $appContainer.append($busstop1).append($busstop2).append($busstop3);
 
-        var sec = parseInt($('.second').text());
-        var timer = setInterval(function () {
-            $('.second').text(--sec);
-            if (sec == 0) {
-                $('.second').fadeOut('fast');
-                $('.finish').delay(500).fadeIn(500);
+    var buses = [bus1Id, bus2Id, bus3Id, bus3Id, bus1Id, bus1Id];
 
-                clearInterval(timer);
-                gameOver = true;
-            }
-
-        }, 1000);
-    }
-
-    button.on('click', setTime);
-
-
-
-
-    function losowanieAutobusow() {
-
-        var getBus = Math.floor((Math.random() * 5) + 1);
-
-        if (getBus == bus1.number) {
-
-            bus1Id.addClass('bus-active');
-            bus1Id.removeClass('bus-no-active');
-
-            setTimeout(function(){
-                bus1Id.removeClass('bus-active');
-                bus1Id.addClass('bus-no-active');
-            }, bus1.timer);
-
-            bus2Id.addClass('bus-no-active').removeClass('bus-active');
-            bus3Id.addClass('bus-no-active').removeClass('bus-active');
-
-
-        } else if (getBus == bus2.number) {
-
-            bus2Id.addClass('bus-active');
-            bus2Id.removeClass('bus-no-active');
-
-            setTimeout(function(){
-                bus2Id.removeClass('bus-active');
-                bus2Id.addClass('bus-no-active');
-            }, bus2.timer);
-
-            bus1Id.addClass('bus-no-active').removeClass('bus-active');
-            bus3Id.addClass('bus-no-active').removeClass('bus-active');
-
-        } else {
-            bus3Id.addClass('bus-active');
-            bus3Id.removeClass('bus-no-active');
-
-            setTimeout(function(){
-                bus3Id.removeClass('bus-active');
-                bus3Id.addClass('bus-no-active');
-            }, bus3.timer);
-
-            bus2Id.addClass('bus-no-active').removeClass('bus-active');
-            bus1Id.addClass('bus-no-active').removeClass('bus-active');
-
-        }
-
-    }
-
-    setInterval(losowanieAutobusow, 3000);
-
-
+function liczeniePunktow(bus){
     $('div').on('click', function () {
         if ($(this).hasClass('bus-no-active')) {
             count -=10;
@@ -178,47 +95,194 @@ $(document).ready(function() {
         }
 
     });
+}
 
-    var $appContainer = $('<div id="app">').css({position: 'relative'});
-    $('body').append($appContainer);
+    //function addBuss(top, left,prize) {
+    //    var $bus = $('<div>').addClass('bus');
+    //    $bus.css({
+    //        background: prize === bus1Id ? $bus.addClass('bus-active') : $bus.addClass('bus-no-active'),
+    //        //width: 100,
+    //        //height: 50,
+    //        position: 'absolute',
+    //        top: top,
+    //        left: left
+    //    });
+    //
+    //    return $bus;
+    //}
+    //
+    //addBuss(200, 200);
+    //
+    //
+    //    //start(
+    //    //addBuss(200, 0)
+    //    //);
 
-    var buses = [1, -10, 1, 1, 1, -10];
+    function start() {
+        buses.forEach(function (prize, index) {
+            setTimeout(function () {
+                var $bus = $('<div>').addClass('bus');
 
-    buses.forEach(function (prize, index) {
-        setTimeout(function () {
-            var $bus = $('<div>');
-            $bus.css({
-                background: prize === -10 ? '#c00' : '#00c',
-                width: 100,
-                height: 50,
-                position: 'absolute',
-                top: 0,
-                left: 0
-            });
+                $bus.css({
+                    background: prize === bus1Id ? $bus.addClass('bus-active') : $bus.addClass('bus-no-active'),
+                    //width: 100,
+                    //height: 50,
+                    position: 'absolute',
+                    top: 40,
+                    left: 0
+                });
+                var $doorLeft = $('<div>').addClass('doorLeft');
+                var $doorRight = $('<div>').addClass('doorRight');
+                $bus.append($doorLeft).append($doorRight);
 
-            $appContainer.append(
-                $bus.animate(
-                    {
-                        left:200
-                    },
-                    4000,
-                    'linear',
-                    function () {
-                        setTimeout(function () {
-                            $bus.animate({
-                                left: 400
-                            }, 4000, 'linear', function () {
-                                $(this).hide(300);
-                            });
-                        }, 1000)
-                    }
-                )
-            );
 
-            console.log(prize);
-        }, 2000 * index + Math.random() * 500)
-    });
+                var $bus1 = $('<div>').addClass('bus');
+                $bus1.css({
+                    background: prize === bus1Id ? $bus1.addClass('bus-active') : $bus1.addClass('bus-no-active'),
+                    //width: 100,
+                    //height: 50,
+                    position: 'absolute',
+                    top: 180,
+                    left: 0
+                });
 
+                $appContainer.append(
+                    $bus.animate(
+                        {
+                            left:400
+                        },
+                        5000, 'linear', function () {
+
+                            $doorLeft.animate({
+                                left: 30
+                            }, 500, 'linear');
+
+                            $doorRight.animate({
+                                left: 100
+                            }, 500, 'linear');
+
+
+                            setTimeout(function(){
+                                $doorLeft.animate({
+                                    left: 60
+                                }, 500, 'linear');
+
+                                $doorRight.animate({
+                                    left: 80
+                                }, 500, 'linear');
+                            }, 1000);
+
+                            setTimeout(function () {
+
+                                $bus.animate({
+                                    left: 800
+                                }, 5000, 'linear', function () {
+                                    $(this).hide(100);
+                                });
+
+                            }, 2000)
+                        }
+                    )
+                );
+
+                $appContainer.append(
+                    $bus1.animate(
+                        {
+                            left:400
+                        },
+                        5000,
+                        'linear',
+                        function () {
+                            setTimeout(function () {
+                                $bus1.animate({
+                                    left: 800
+                                }, 5000, 'linear', function () {
+                                    $(this).hide(100);
+                                });
+                            }, 2000)
+                        }
+                    )
+                );
+
+                liczeniePunktow($('.bus'));
+
+                console.log(prize);
+            }, 5000 * index + Math.random() * 500);
+
+        });
+
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+   // function start() {
+   // buses.forEach(function (prize, index) {
+   //     setTimeout(function () {
+   //         var $bus = $('<div>').addClass('bus');
+   //         $bus.css({
+   //             background: prize === bus1Id ? $bus.addClass('bus-active') : $bus.addClass('bus-no-active'),
+   //             //width: 100,
+   //             //height: 50,
+   //             position: 'absolute',
+   //             top: 0,
+   //             left: 0
+   //         });
+   //
+   //
+   //         $appContainer.append(
+   //             $bus.animate(
+   //                 {
+   //                     left:400
+   //                 },
+   //                 5000,
+   //                 'linear',
+   //                 function () {
+   //                     setTimeout(function () {
+   //                         $bus.animate({
+   //                             left: 800
+   //                         }, 5000, 'linear', function () {
+   //                             $(this).hide(100);
+   //                         });
+   //                     }, 2000)
+   //                 }
+   //             )
+   //         );
+   //
+   //         liczeniePunktow($('.bus'));
+   //
+   //         console.log(prize);
+   //     }, 5000 * index + Math.random() * 500);
+   //
+   // });
+   //
+   //}
+
+    start();
 
 });
 
--- js/gamebus_probny.js
+++ js/gamebus_probny.js
@@ -214,8 +214,238 @@ $(document).ready(function() {
 
 });
 
+ //wersja 1************************************************************************************************
+
+//
+//$(document).ready(function() {
+//
+//    gameOver = false;
+//
+//    var bus1 = {
+//        number: 1,
+//        timer: Math.floor(((Math.random() * 4) + 1)* 1000),
+//        passengers: '',
+//        points: ''
+//
+//    };
+//
+//    var bus2 = {
+//        number: 2,
+//        timer: Math.floor(((Math.random() * 4) + 1)* 1000),
+//        passengers: '',
+//        points: ''
+//    };
+//
+//    var bus3 = {
+//        number: 3,
+//        timer: Math.floor(((Math.random() * 4) + 1)* 1000),
+//        passengers: '',
+//        points: ''
+//    };
+//
+//    bus1Id = $('#bus1');
+//    bus2Id = $('#bus2');
+//    bus3Id = $('#bus3');
+//
+//    var busstop = {
+//        number: '1',
+//        ifThereisBus: false
+//    };
+//
+//    buses = [bus1, bus2, bus3];
+//
+//    gameOver = false;
+//    var button = $('input');
+//    var count = 0;
+//
+//
+//    function goToBusStation1() {
+//        bus1Id.animate({
+//            left: 200
+//        }, bus1.timer, 'linear', goBack1);
+//        setTimeout(goToBusStation1, 2000);
+//    }
+//    function goBack1() {
+//        bus1Id.animate({
+//            left:400
+//        }, bus1.timer, 'linear');
+//        bus1Id.animate({
+//            left: 0
+//        }, 2000, 'linear');
+//    }
+//
+//
+//    function goToBusStation2() {
+//        bus2Id.animate({
+//            left: 200
+//        }, bus2.timer, 'linear', goBack2);
+//        setTimeout(goToBusStation2, 2000);
+//    }
+//    function goBack2() {
+//        bus2Id.animate({
+//            left:400
+//        }, bus2.timer, 'linear');
+//        bus2Id.animate({
+//            left: 0
+//        }, 2000, 'linear');
+//    }
+//
+//    function goToBusStation3() {
+//        bus3Id.animate({
+//            left: 200
+//        }, bus3.timer, 'linear', goBack3);
+//        setTimeout(goToBusStation3, 2000);
+//    }
+//    function goBack3() {
+//        bus3Id.animate({
+//            left:400
+//        }, bus3.timer, 'linear');
+//        bus3Id.animate({
+//            left: 0
+//        }, 2000, 'linear');
+//    }
+//
+//
+//    goToBusStation1();
+//    goToBusStation2();
+//    goToBusStation3();
+//
+//
+//
+//
+//
+//    function setTime() {
+//
+//        var sec = parseInt($('.second').text());
+//        var timer = setInterval(function () {
+//            $('.second').text(--sec);
+//            if (sec == 0) {
+//                $('.second').fadeOut('fast');
+//                $('.finish').delay(500).fadeIn(500);
+//
+//                clearInterval(timer);
+//                gameOver = true;
+//            }
+//
+//        }, 1000);
+//    }
+//
+//    button.on('click', setTime);
+//
+//
+//
+//
+//    function losowanieAutobusow() {
+//
+//        var getBus = Math.floor((Math.random() * 5) + 1);
+//
+//        if (getBus == bus1.number) {
+//
+//            bus1Id.addClass('bus-active');
+//            bus1Id.removeClass('bus-no-active');
+//
+//            setTimeout(function(){
+//                bus1Id.removeClass('bus-active');
+//                bus1Id.addClass('bus-no-active');
+//            }, bus1.timer);
+//
+//            bus2Id.addClass('bus-no-active').removeClass('bus-active');
+//            bus3Id.addClass('bus-no-active').removeClass('bus-active');
+//
+//
+//        } else if (getBus == bus2.number) {
+//
+//            bus2Id.addClass('bus-active');
+//            bus2Id.removeClass('bus-no-active');
+//
+//            setTimeout(function(){
+//                bus2Id.removeClass('bus-active');
+//                bus2Id.addClass('bus-no-active');
+//            }, bus2.timer);
+//
+//            bus1Id.addClass('bus-no-active').removeClass('bus-active');
+//            bus3Id.addClass('bus-no-active').removeClass('bus-active');
+//
+//        } else {
+//            bus3Id.addClass('bus-active');
+//            bus3Id.removeClass('bus-no-active');
+//
+//            setTimeout(function(){
+//                bus3Id.removeClass('bus-active');
+//                bus3Id.addClass('bus-no-active');
+//            }, bus3.timer);
+//
+//            bus2Id.addClass('bus-no-active').removeClass('bus-active');
+//            bus1Id.addClass('bus-no-active').removeClass('bus-active');
+//
+//        }
+//
+//    }
+//
+//    setInterval(losowanieAutobusow, 3000);
 
 
+
+
+//wersja4******************************************************
+//
+//button.on('click', setTime);
+//
+//$('div').on('click', function () {
+//    if ($(this).hasClass('bus-no-active')) {
+//        count -=10;
+//        $('#points').html("Punkty: " + count);
+//    }
+//
+//    if ($(this).hasClass('bus-active')) {
+//        count += 1;
+//        $('#points').html("Punkty: " + count);
+//    }
+//
+//});
+//
+//var $appContainer = $('<div id="app">').css({position: 'relative'});
+//$('body').append($appContainer);
+//
+//var buses = [1, -10, 1, 1, 1, -10];
+//
+//buses.forEach(function (prize, index) {
+//    setTimeout(function () {
+//        var $bus = $('<div>');
+//        $bus.css({
+//            background: prize === -10 ? '#c00' : '#00c',
+//            width: 100,
+//            height: 50,
+//            position: 'absolute',
+//            top: 0,
+//            left: 0
+//        });
+//
+//        $appContainer.append(
+//            $bus.animate(
+//                {
+//                    left:200
+//                },
+//                4000,
+//                'linear',
+//                function () {
+//                    setTimeout(function () {
+//                        $bus.animate({
+//                            left: 400
+//                        }, 4000, 'linear', function () {
+//                            $(this).hide(300);
+//                        });
+//                    }, 1000)
+//                }
+//            )
+//        );
+//
+//        console.log(prize);
+//    }, 2000 * index + Math.random() * 500)
+//});
+//
+//
+//});
 
 
 
