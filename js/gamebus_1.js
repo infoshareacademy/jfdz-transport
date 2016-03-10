8d3231c1c5d51d88d51a5a393082fc6e0f3bb568
kamilazabik <kamilazabik@gmail.com>
2016-03-10 21:37:02 +0100

    gra

--- js/gamebus_1.js
+++ js/gamebus_1.js
@@ -1,27 +1,18 @@
 $(document).ready(function() {
 
-    gameOver = false;
-
     var bus1 = {
         number: 1,
-        timer: Math.floor(((Math.random() * 4) + 1)* 1000),
-        passengers: '',
-        points: ''
-
+        timer: Math.floor(((Math.random() * 4) + 1)* 1000)
     };
 
     var bus2 = {
         number: 2,
-        timer: Math.floor(((Math.random() * 4) + 1)* 1000),
-        passengers: '',
-        points: ''
+        timer: Math.floor(((Math.random() * 4) + 1)* 1000)
     };
 
     var bus3 = {
         number: 3,
-        timer: Math.floor(((Math.random() * 4) + 1)* 1000),
-        passengers: '',
-        points: ''
+        timer: Math.floor(((Math.random() * 4) + 1)* 1000)
     };
 
     bus1Id = $('#bus1');
@@ -31,20 +22,12 @@ $(document).ready(function() {
     $startButton = $('#startGameButton');
     $stopButtom = $('#stopGameButton');
 
-    var busstop = {
-        number: '1',
-        ifThereisBus: false
-    };
-
     var state = {
         score: 0,
-        time: 15
+        time: 25
     };
 
-    buses = [bus1, bus2, bus3];
-
     gameOver = false;
-    var count = 0;
 
     function displayClock(node, state) {
         $(node).text('Czas: ' + state.time);
@@ -59,26 +42,25 @@ function startGame(initialState) {
     setTimeout(function () {
         clearInterval(clockIntervalId);
     }, state.time * 1000);
-
-
 }
 
+
     $startButton.click(function(){
+        state.score = 0;
+        $('#points').html("Punkty: " + state.score);
+        state.time = 26;
         start(state);
         startGame(state);
+        gameOver = true;
 
     });
 
-
     displayClock($clock, state);
-
 
     $stopButtom.click(function(){
         start.stop();
 
     });
-
-
 
     var $appContainer = $('<div id="app">').css({position: 'relative'}).addClass('plansza');
     var $busstop1 = $('<div id="busstop">').addClass('przystanek').addClass('przystanek1');
@@ -91,65 +73,61 @@ function startGame(initialState) {
 
     var buses = [bus1Id, bus2Id, bus3Id, bus3Id, bus1Id, bus1Id];
 
-function liczeniePunktow(bus){
-    $('div').on('click', function () {
-        if ($(this).hasClass('bus-no-active')) {
-            state.score -=10;
-            $('#points').html("Punkty: " + state.score);
-        }
+//function liczeniePunktow(bus){
+//    $('div').on('click', function () {
+//        if ($(this).hasClass('bus-no-active') ) {
+//            state.score -=10;
+//            $('#points').html("Punkty: " + state.score);
+//        }
+//
+//        if ($(this).hasClass('bus-active')) {
+//            state.score += 1;
+//            $('#points').html("Punkty: " + state.score);
+//        }
+//
+//    });
+//}
 
-        if ($(this).hasClass('bus-active')) {
-            state.score += 1;
-            $('#points').html("Punkty: " + state.score);
-        }
+    function liczeniePunktow(bus){
+        $('div').on('click', function () {
+            if ($(this).hasClass('doorClose') ) {
+                state.score -=10;
+                $('#points').html("Punkty: " + state.score);
+            }
 
-    });
-}
+            if ($(this).hasClass('doorOpen')) {
+                state.score += 1;
+                $('#points').html("Punkty: " + state.score);
+            }
 
-    //function addBuss(top, left,prize) {
-    //    var $bus = $('<div>').addClass('bus');
-    //    $bus.css({
-    //        background: prize === bus1Id ? $bus.addClass('bus-active') : $bus.addClass('bus-no-active'),
-    //        //width: 100,
-    //        //height: 50,
-    //        position: 'absolute',
-    //        top: top,
-    //        left: left
-    //    });
-    //
-    //    return $bus;
-    //}
-    //
-    //addBuss(200, 200);
-    //
-    //
-    //    //start(
-    //    //addBuss(200, 0)
-    //    //);
+        });
+    }
+
 
     function start() {
         buses.forEach(function (prize, index) {
+
             setTimeout(function () {
                 var $bus = $('<div>').addClass('bus');
 
                 $bus.css({
-                    background: prize === bus1Id ? $bus.addClass('bus-active') : $bus.addClass('bus-no-active'),
-                    //width: 100,
-                    //height: 50,
+                    //background: prize === bus1Id ? $bus.addClass('bus-active') : $bus.addClass('bus-no-active'),
                     position: 'absolute',
                     top: 40,
                     left: 0
                 });
                 var $doorLeft = $('<div>').addClass('doorLeft');
                 var $doorRight = $('<div>').addClass('doorRight');
-                $bus.append($doorLeft).append($doorRight);
+                var $afterOpeningDoor = $('<div>');
+                $afterOpeningDoor.css ({
+                    background: prize === bus1Id ? $afterOpeningDoor.addClass('doorOpen') : $afterOpeningDoor.addClass('doorClose')
+                });
+                $bus.append($doorLeft).append($doorRight).append($afterOpeningDoor);
 
 
                 var $bus1 = $('<div>').addClass('bus');
                 $bus1.css({
-                    background: prize === bus1Id ? $bus1.addClass('bus-active') : $bus1.addClass('bus-no-active'),
-                    //width: 100,
-                    //height: 50,
+                    //background: prize === bus1Id ? $bus1.addClass('bus-active') : $bus1.addClass('bus-no-active'),
                     position: 'absolute',
                     top: 180,
                     left: 0
@@ -169,7 +147,6 @@ function liczeniePunktow(bus){
                             $doorRight.animate({
                                 left: 100
                             }, 500, 'linear');
-
 
                             setTimeout(function(){
                                 $doorLeft.animate({
@@ -213,7 +190,8 @@ function liczeniePunktow(bus){
                     )
                 );
 
-                liczeniePunktow($('.bus'));
+                //liczeniePunktow($('.bus'));
+                liczeniePunktow($afterOpeningDoor);
 
                 console.log(prize);
             }, 5000 * index + Math.random() * 500);
