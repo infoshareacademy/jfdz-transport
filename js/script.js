09d5cfbe060bff0b1128c5d967add9690b98f424
MarcinWirkus <marcin@inkre.pl>
2016-04-16 17:46:29 +0200

    walidacja js

--- js/script.js
+++ js/script.js
@@ -110,6 +110,38 @@ $(document).ready(function(){
     }
 });
 
+/**
+ * Created by agniecha on 2016-02-16.
+ */
+
+
+$(window).scroll(function () {
+    var wScroll = $(this).scrollTop();
+    $('.welcome img').css({
+        'transform': 'translate(0px, ' + wScroll / 20 + '%)'
+    });
+    $('.slogan').css({
+        'transform': 'translate(0px, ' + wScroll / 20 + '%)'
+    });
+    //$('.slogan p').css({
+    //    'transform': 'translate(0px, '+ wScroll/4+ '%)'
+    //});
+
+    if (wScroll > $('.features-container').offset().top -
+        $(window).height()) {
+        var offset = Math.min(0, wScroll -
+            $('.features-container').offset().top + $(window).height() - 200);
+        $('.feature-image-1').css({
+            'transform': 'translate(-' +
+            Math.abs(offset) + 'px ,' + 0 + 'px)'
+        });
+        $('.feature-image-3').css({
+            'transform': 'translate(' +
+            Math.abs(offset) + 'px ,' + 0 + 'px)'
+        });
+    }
+});
+
 
 function getGETParameter(key)
 {
